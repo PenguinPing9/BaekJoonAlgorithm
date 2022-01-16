6f3971c803aa456b76961d2f301c772b13c565ec
PinG9 <46869486+PenguinPing9@users.noreply.github.com>
2022-01-16 14:23:01 +0900

    Create baekjoon2750.java

---
+++ BaekJoon/src/BaekJoon/baekjoon2750.java
@@ -0,0 +1,25 @@
+package BaekJoon;
+import java.util.*;
+
+public class baekjoon2750 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int n = scanner.nextInt();
+		int[] arr = new int[n];
+		
+		for(int i = 0; i < n; i++)
+			arr[i] = scanner.nextInt();
+		
+		for(int i = 0; i < n - 1; i++)
+			for(int j = 0; j < n - 1; j++) 
+				if(arr[j] > arr[j+1]) {
+					int tmp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = tmp;
+				}
+		
+		for(int i = 0; i < n; i++)
+			System.out.println(arr[i]);
+	}
+}
